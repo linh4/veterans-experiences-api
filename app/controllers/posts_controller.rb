1be73407cc0a613a60c408db003b1c6d435ea53f
Patrick Abejar <wireless.patrick@gmail.com>
2018-12-08 14:41:43 -0500

    created tagpost on tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,12 +14,18 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     params[:content].split.map{|tag| tag.gsub(/[^a-z ]/i, '').downcase}.each do |tag|
-      tag = Tag.find_by(tag: tag)
-      if tag
-        tag.increment_count
-      else
-        Tag.create(tag: tag, count: 0)
-      end
+      newly_created_tag = Tag.find_or_create_by(tag: tag.downcase)
+      TagPost.create(tag_id: newly_created_tag.id, post_id: self.id)
+
+      # tag = Tag.find_by(tag: tag)
+      # if tag
+      #   tag.increment_count
+      # else
+      #   Tag.create(tag: tag, count: 0)
+      # end
+      #
+      #
+      # tag = Tag.find_by()
     end
 
     if @post.save
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,11 @@ class Tag < ApplicationRecord
   has_many :tag_posts
   has_many :posts, through: :posts
 
-  def increment_count
-    count += 1
+  # def increment_count
+  #   count += 1
+  # end
+
+  def self.count_tags
+    Tag.find_by(tag: self.tag).length
   end
 end
